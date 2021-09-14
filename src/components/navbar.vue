25d0779741be1d3e8dafb1375cf11e6a04e5f5d6
LAPTOP-2GI4N8QM\c_bri <catalin.briscan@principal3.com>
2021-09-14 08:08:19 +0300

    Added the navbar

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,16 +1,21 @@
 <template>
-    <div class="headerRight">
-        <router-link class="menu-item _menu _link" to="/">Gmail</router-link>
-        <router-link class="menu-item _menu _link" to="/">Images</router-link>
-        <div class="menu-item _menu">
-            <v-icon color="blue-grey darken-2">mdi-apps</v-icon>
-        </div>
-        <div class="menu-=item">
-            <v-icon x-large color="blue-grey darken-2">mdi-account-circle</v-icon>
-        </div>
+  <div class="headerRight">
+    <router-link class="menu-item _menu _link" to="/">Gmail</router-link>
+    <router-link class="menu-item _menu _link" to="/">Images</router-link>
+    <div class="menu-item _menu">
+      <a href="#" class="image">
+        <svg class="menu" focusable="false" viewBox="0 0 24 24">
+          <path
+            d="M6,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM16,6c0,1.1 0.9,2 2,2s2,-0.9 2,-2 -0.9,-2 -2,-2 -2,0.9 -2,2zM12,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2z"
+          ></path>
+        </svg>
+      </a>
     </div>
+    <div class="menu-item">
+      <a href="#" class="image"><img src="../assets/me.png" class="myphoto"/></a>
+    </div>
+  </div>
 </template>
-
 <style scoped>
 .headerRight {
   display: flex;
@@ -41,4 +46,30 @@ a {
 a:hover {
   text-decoration: underline;
 }
+
+.menu {
+  height: 24px;
+  width: 24px;
+  fill: rgb(95, 99, 104);
+}
+
+.myphoto {
+  border-radius: 50%;
+  padding: 5px;
+  width: 30px;
+}
+
+.image {
+  padding: 5px;
+  width: 20px;;
+  padding-bottom: 1px;
+  border: transparent;
+}
+
+.image:hover {
+  box-shadow: 0px 1px 5px #949494;
+  background-color: rgba(60, 64, 67, 0.08);
+  border-radius: 50%;
+}
+
 </style>
